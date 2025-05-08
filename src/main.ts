e3262e9f16f59883e6dab69e52b6bc0a1d50aea5
AronJustoC <aronchoque727@gmail.com>
2025-05-08 10:44:38 -0500

    feat: add express-rate-limit for request throttling

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,20 @@ import todoRoutes from "./routes/todo-list.route";
 import userRoutes from "./routes/user.route";
 import authRoutes from "./routes/auth.route";
 import { validateToken } from "./middleware/jwt.middleware";
+import { rateLimit } from "express-rate-limit";
 
 const port = process.env.PORT || 4000;
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 app.use(bodyParser.json());
+app.use(limiter);
 
 app.use("/todos", validateToken, todoRoutes);
 app.use("/users", validateToken, userRoutes);
